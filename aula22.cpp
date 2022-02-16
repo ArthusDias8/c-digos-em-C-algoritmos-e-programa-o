8217a347f3aac4d0b9b4eb1d5be239616f3cd267
ArthusDias8 <99662029+ArthusDias8@users.noreply.github.com>
2022-02-16 11:22:36 -0300

    Add files via upload

---
+++ aula22.cpp
@@ -0,0 +1,17 @@
+//pontos flutuantes = numeros reais
+//float = 4bytes %f
+//double = 8bytes %lf
+
+#include<stdio.h>
+#define M1 3.5
+#define M2 7.5
+
+int main (){
+	double a, b, x;
+	printf ("Usuário, digite o primeiro número: ");
+	scanf("%lf", &a);
+	printf ("Usuário, digite o segundo número: ");
+	scanf("%lf", &b);
+	x = ((a * M1) + (b * M2))/11;
+	printf("X = %.3lf", x);
+}
