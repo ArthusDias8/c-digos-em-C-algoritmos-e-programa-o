29b56247fa88ca495c8ad56138cd1839536e1192
ArthusDias8 <99662029+ArthusDias8@users.noreply.github.com>
2022-02-16 09:22:42 -0300

    Add files via upload

---
+++ aula2.cpp
@@ -0,0 +1,15 @@
+/*Your first program in any programming 
+language is usually "Hello World!". 
+In this first problem all you 
+have to do is print this message on the screen.*/
+//comntando uma única linha
+
+#include<stdio.h> //biblioteca para fumção de entrada e saida de dados
+
+int main(){ //inicio do algoritmo
+
+    printf("Hello World!");
+    
+}//final do algoritmo
+
+
---
+++ aula22.cpp
@@ -0,0 +1,15 @@
+//pontos flutuantes = numeros reais
+//float = 4bytes %f
+//double = 8bytes %lf
+
+#include<stdio.h>
+
+int main (){
+	int a, b, x;
+	printf ("Usuário, digite o primeiro número: ");
+	scanf("%lf", &a);
+	printf ("Usuário, digite o segundo número: ");
+	scanf("%lf", &b);
+	x = ((a * 3.5) + (b * 7.5))/11;
+	printf("X = %lf", X);
+}
